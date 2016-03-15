b812b8c6a500dee5af76f6d87b30e1940798a735
donghwan <donghwan_seo@tmax.co.kr>
2016-03-15 10:59:12 +0900

    add test execute script

---
+++ runTest.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+$JAVA7/bin/java \
+ 	-classpath ./jms-performance-test/target/jms-performance-test-1.0-SNAPSHOT.jar:$JEUS_HOME/lib/client/jclient.jar:$JEUS_HOME/lib/system/jms.jar \
+	-Dexamples.test.duration=300000			\
+	-Dexamples.test.warm-up-time=30000		\
+	-Dexamples.test.message-size=2048 		\
+	-Dexamples.test.send-session-count=2		\
+	-Dexamples.test.receive-session-count=2	\
+	-Dexamples.test.produce-interval=5 		\
+	-Dexamples.test.ring-buffer-size=1024		\
+	examples.test.TestExecutor
+
